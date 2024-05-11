357266069f25e82a491035359bef2e38e9525eac
ennarsh <gennaro.liguori.sh@gmail.com>
2024-05-11 19:33:03 +0200

    'release'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-   apiUrl: 'https://9570-79-52-136-128.ngrok-free.app',
+   apiUrl: 'https://5039-79-52-136-128.ngrok-free.app',
   paypalClientId: 'Aa68SHz_t56IwRP0u2Gch6HN6UnkXk6saKE56OqPqm14Ok3L6tsXeTBoQrMZ-ff7YZ5Z0G1SIWF9LUp1'
 };
